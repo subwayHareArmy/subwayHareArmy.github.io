7da4d3b449a37f91768a79d546903609fa1c0dba
Ayush Yembarwar <ayush.yembarwar@ril.com>
2023-10-29 09:55:41 +0530

    Added dark theme, added JS - Astro <script> and Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://subwayharearmy.netlify.app"
+  site: "https://subwayharearmy.netlify.app",
+  integrations: [preact()]
 });
-  
