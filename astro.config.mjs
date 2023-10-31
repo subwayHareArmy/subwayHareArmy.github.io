972d8fcf626fc45c33464ad78da4f60ae5a3ff91
Ayush Yembarwar <ayush.yembarwar@ril.com>
2023-10-31 12:20:25 +0530

    Added GoogleAnalytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-
 import { autolinkConfig } from "./plugins/rehype-autolink-config";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://subwayharearmy.github.io",
-  integrations: [react(), mdx(), sitemap()],
+  integrations: [react(), mdx(), sitemap(), partytown()],
   markdown: {
     rehypePlugins: [rehypeHeadingIds, [rehypeAutolinkHeadings, autolinkConfig]],
   },
